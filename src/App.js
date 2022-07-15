a60935b6bbd4ba70094836f9b0c537dee48c7511
MirHeyder03 <mhyahyayev03@gmail.com>
2022-07-15 17:56:41 +0400

    first-part added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import "./App.scss";
+import apis from "./api/cards.json";
+import SearchBar from "./components/search/SearchBar";
+import { useState,useEffect } from "react";
+import Cards from "./components/Cards/Cards";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [api, setApi] = useState(apis);
+
+
+  useEffect(()=>{
+      
+  },[search])
+  
   return (
     <div className="App">
-
+      <h3>
+        A collective list of free APIs for use in <br /> software and web
+        development.
+      </h3>
+      <SearchBar search={search} setSearch={setSearch} />
+      <div className="container">
+        <h3>Featured APIs of this week</h3>
+        <div className="api-list">
+            {
+              apis.map(api=>(
+                <Cards api={api} key={api.id}/>
+              ))
+            }
+        </div>
+      </div>
     </div>
   );
 }
